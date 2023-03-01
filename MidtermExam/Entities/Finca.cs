a16a1d505906f1d93ad81b5f38acd8d2cfd15600
Guilder W. Milliner <guilderw@gmail.com>
2023-02-28 18:37:06 -0600

    added Entity

---
+++ MidtermExam/Entities/Finca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities
+{
+    public partial class Finca
+    {
+        public Finca()
+        {
+            Propietarios = new HashSet<Propietario>();
+        }
+
+        public int FincaId { get; set; }
+        public string Numero { get; set; } = null!;
+        public int? Distrito { get; set; }
+        public string? Direccion { get; set; }
+
+        public virtual ICollection<Propietario> Propietarios { get; set; }
+    }
+}
---
+++ MidtermExam/Entities/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities
+{
+    public partial class Persona
+    {
+        public Persona()
+        {
+            Propietarios = new HashSet<Propietario>();
+        }
+
+        public int PersonaId { get; set; }
+        public string? Cedula { get; set; }
+        public string? Nombre { get; set; }
+
+        public virtual ICollection<Propietario> Propietarios { get; set; }
+    }
+}
